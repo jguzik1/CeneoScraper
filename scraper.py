2694a60082c621ca38e8b69a7071ce6a64f8b72d
jguzik1 <227616@student.uek.krakow.pl>
2023-04-05 14:52:14 +0200

    modified screaper

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_cos(ancestor, selector):
+    return ancestor.select_one(selector).text.strip(),
+
 # product_code = input("Podaj kod produktu: ")
 product_code = "95319759"
 # url = "https://www.ceneo.pl/" + product_code + "#tab=reviews"
@@ -9,5 +12,23 @@ url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
 opinions = page_dom.select("div.js_product-review")
+all_opinions = []
 for opinion in opinions:
-    print(opinion["data-entry-id"])
+    single_opinion ={
+        "opinion" : opinion["data-entry-id"],
+        "author" : opinion.select_one("span.user-post__author-name").text.strip(),
+        "recommendaton" : opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
+        "stars" : opinion.select_one("span.user-post__score-count").text.strip(),
+        "purchased" : opinion.select_one("div.review-pz").text.strip(),
+        "opinion_date" : opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
+        "purchase_date" : opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
+        "useful" : opinion.select_one("button.vote-yes")["data-total-vote"].strip(),
+        "unuseful" : opinion.select_one("button.vote-no > span").text.strip(),
+        "content" : opinion.select_one("div.user-post__text").text.strip(),
+        "cons" : [cons.text.strip()for cons in opinion.select_one("div.review-feature__title--negatives ~ div.review-feature__item").text.strip()],
+        "pros" : [pros.text.strip() for pros in opinion.select_one("div.review-feature__title--positives ~ div.review-feature__item").text.strip()],
+
+    }
+    all_opinions.append(single_opinion)
+print(all_opinions)
+    
