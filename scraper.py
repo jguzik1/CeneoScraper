00e4778d54f8ad918ceae01dadd40638967b1acb
jguzik1 <227616@student.uek.krakow.pl>
2023-04-12 13:00:20 +0200

    noidea

--- scraper.py
+++ scraper.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_cos(ancestor, selector):
-    return ancestor.select_one(selector).text.strip(),
+def get_element(ancestor, selector = None, attribute = None, return_list = False):
+    try:
+        if return_list:
+            return [tag.text.strip() for tag in ancestor.select(selector)].copy()
+        if not selector:
+            return ancestor[attribute]
+        if attribute:
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.select_one(selector).text.strip()
+    except AttributeError:
+        return None
 
 # product_code = input("Podaj kod produktu: ")
 product_code = "95319759"
@@ -16,15 +25,15 @@ all_opinions = []
 for opinion in opinions:
     single_opinion ={
         "opinion" : opinion["data-entry-id"],
-        "author" : opinion.select_one("span.user-post__author-name").text.strip(),
-        "recommendaton" : opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
+        "author" : get_element(opinion,"span.user-post__author-name"),
+        "recommendaton" : get_element(opinion,"span.user-post__author-recomendation > em"),
         "stars" : opinion.select_one("span.user-post__score-count").text.strip(),
-        "purchased" : opinion.select_one("div.review-pz").text.strip(),
-        "opinion_date" : opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
-        "purchase_date" : opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
-        "useful" : opinion.select_one("button.vote-yes")["data-total-vote"].strip(),
-        "unuseful" : opinion.select_one("button.vote-no > span").text.strip(),
-        "content" : opinion.select_one("div.user-post__text").text.strip(),
+        "purchased" : get_element(opinion,"div.review-pz"),
+        "opinion_date" : get_element(opinion,"span.user-post__published > time:nth-child(1)", "datetime"),
+        "purchase_date" : get_element(opinion,"span.user-post__published > time:nth-child(2)", "datetime"),
+        "useful" : get_element(opinion, "button.vote-yes", "data-total-vote"),
+        "unuseful" : get_element(opinion, "button.vote-no > span"),
+        "content" : get_element(opinion,"div.user-post__text"),
         "cons" : [cons.text.strip()for cons in opinion.select_one("div.review-feature__title--negatives ~ div.review-feature__item").text.strip()],
         "pros" : [pros.text.strip() for pros in opinion.select_one("div.review-feature__title--positives ~ div.review-feature__item").text.strip()],
 
